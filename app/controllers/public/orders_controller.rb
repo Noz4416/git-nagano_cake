c5ef9dff59f5be74655f8cc7460785a4b29109a0
EC2 Default User <104708989+Noz4416@users.noreply.github.com>
2022-06-17 15:00:51 +0000

    AddOrderPage

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,12 +9,25 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @cart_items = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items
     @total = @cart_items.inject(0) {|sum, item| sum + item.sum_of_price}
     @total_payment = @total + @order.shipping_cost
-    session[:payment_method] = @order.payment_method
-    session[:address_number] = params[:address_number]
-
+    @order.payment_method = params[:order][:payment_method]
+    if params[:order][:address_number] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.customer_name
+    elsif params[:order][:address_number] == "2"
+      @addresses = params[:order][:address].to_i
+      @address = Address.find(@addresses)
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:address_number] == "3"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    end
   end
 
 
@@ -23,9 +36,13 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @order.customer_id = current_customer.id
+    @order = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details = Order.order_details
   end
 
   private

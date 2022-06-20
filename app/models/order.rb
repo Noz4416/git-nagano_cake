502209b5e66ff1937f45bb67b0162d99f4bff157
EC2 Default User <104708989+Noz4416@users.noreply.github.com>
2022-06-20 14:29:09 +0000

    AddStatus

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ApplicationRecord
 
   enum payment_method: {credit_card: 0, transfer: 1}
 
-  enum status: {waiting: 0, confirmation: 1, production: 2,ready: 3,sent: 4}
+  enum status: {waiting: 0, confirmation: 1, production: 2, ready: 3, sent: 4}
 
 
   def shipping_cost
@@ -14,6 +14,15 @@ class Order < ApplicationRecord
 
   def sum_of_price
     item.tax_in_price * amount
+  end
+
+
+  after_update do
+    # 受注ステータスが入金確認になったら
+    if self.status == "confirmation"
+    # 自動的に製作ステータスを製作待ちに変更する
+      self.order_details.each {|order_detail|order_detail.update(making_status: "waiting")}
+    end
   end
 
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,14 +3,29 @@ class OrderDetail < ApplicationRecord
     belongs_to :item
     belongs_to :order
 
-    enum making_status: {impossible: 0, waiting: 1,production: 2,complete: 3}
+    enum making_status: {impossible: 0, waiting: 1, production: 2, complete: 3}
 
     def tax_in_price
       (price * 1.1).floor
     end
 
+
     def sum_of_price
       tax_in_price * amount
+    end
+
+
+    after_update do
+      order_details = self.order.order_details
+      # 条件分岐:製作ステータスが1つでも製作中になったら
+      if order_details.any? {|order_detail|order_detail.making_status == "production"} == true
+      # 自動的に受注ステータスを製作中に変更する
+        self.order.update(status: "production")
+      # 条件分岐:製作ステータスが全て製作完了だったら
+      elsif order_details.all?{|order_detail|order_detail.making_status == "complete"} == true
+      # 自動的に受注ステータスを発送準備中に変更する
+        self.order.update(status: "ready")
+      end
     end
 
     validates :amount, presence: true

5f5d846002be1b222aca7145ae310109fe31a860
EC2 Default User <104708989+Noz4416@users.noreply.github.com>
2022-06-21 14:06:22 +0000

    FixTest

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,6 +4,14 @@ class Public::SessionsController < Devise::SessionsController
 
   before_action :customer_state, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    public_customers_mypage_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   protected
 
   # 退会しているかを判断するメソッド
